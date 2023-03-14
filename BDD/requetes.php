c1c958bc90f2a584368cab4d4e8057548dfd2549
Esteban Racine <esteban.racineecole@gmail.com>
2023-03-14 11:49:34 +0100

    Prepa addUsers

--- BDD/requetes.php
+++ BDD/requetes.php
@@ -9,11 +9,20 @@ function getAllProduits(){
     return $requete->fetchAll(PDO::FETCH_ASSOC);
 }
 
-
-function AjoutVente($id):void{
+function getProduitById($id):array{
     $connexion = createConnexion();
-    $requete = $connexion->prepare("UPDATE produits SET nbVentes = nbVentes+1 WHERE id = :id");
+    $requete = $connexion->prepare("SELECT * FROM produits WHERE id = :id");
+    $requete -> bindValue('id', $id);
+    $requete -> execute();
+    return $requete->fetch(PDO::FETCH_ASSOC);
+}
+
+
+function AjoutVente($id, $quantite):void{
+    $connexion = createConnexion();
+    $requete = $connexion->prepare("UPDATE produits SET nbVentes = nbVentes+ :quantite WHERE id = :id");
     $requete->bindValue('id', $id);
+    $requete->bindValue('quantite', $quantite);
     $requete->execute();
 }
 
@@ -26,10 +35,32 @@ function getAvisById($id):array{
     return $requete->fetchAll(PDO::FETCH_ASSOC);
 }
 
-function getProduitById($id):array{
+function addUser($nom, $prenom, $mail, $login, $password, $dateNaissance, $ville):bool{
     $connexion = createConnexion();
-    $requete = $connexion->prepare("SELECT * FROM produits WHERE id = :id");
-    $requete -> bindValue('id', $id);
-    $requete -> execute();
-    return $requete->fetch(PDO::FETCH_ASSOC);
+    $sel = "CarottosSel";
+    $requeteSQL = "INSERT INTO users(login, password, nomUser, prenomUser, dateNaissance, villeUser, mailUser) VALUES (:login, :password, :nom, :prenom, :dateNaissance, :ville, :mail)";
+    $requete = $connexion-> prepare($requeteSQL);
+    $password = md5($password.$sel);
+    $requete->bindValue("login", $login);
+    $requete->bindValue("password", $password);
+    $requete->bindValue("nom", $nom);
+    $requete->bindValue("prenom", $prenom);
+    $requete->bindValue("dateNaissance", $dateNaissance);
+    $requete->bindValue("ville", $ville);
+    $requete->bindValue("mail", $mail);
+    return $requete->execute();
+}
+
+function verifLogin($login){
+    $login = trim($login);
+    $connexion =createConnexion();
+    $requete = $connexion->prepare("SELECT login FROM users");
+    $requete->execute();
+    $logins = $requete->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($logins as $log){
+        if ($log['login']==$login){
+            return False;
+        }
+    }
+    return True;
 }
--- createUser.php
+++ createUser.php
@@ -1,1 +1,65 @@
 <?php
+
+session_start();
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+
+}
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Creer un compte</title>
+</head>
+<body>
+<div class="container">
+    <?php
+    include "fichierCommuns/header.php";
+    ?>
+
+    <div class="createUser">
+        <h1><span class="VertContact">C</span>reez un compte chez nous ;)</h1>
+        <form action="" method="post" autocomplete="off">
+            <label for="nom">Nom <span class="Rouge">*</span></label>
+            <input type="text" id="nom" name="nom">
+
+            <label for="prenom">Prénom <span class="Rouge">*</span></label>
+            <input type="text" name="prenom" id="prenom">
+
+            <label for="login">Login <span class="Rouge">*</span></label>
+            <input type="text" name="login" id="login">
+
+            <label for="password">Mot de passe <span class="Rouge">*</span></label>
+            <input type="password" name="password" id="password">
+
+            <label for="ville">Ville <span class="Rouge">*</span></label>
+            <input type="text" name="ville" id="ville">
+
+            <label for="mail">Adresse email <span class="Rouge">*</span></label>
+            <input type="text" name="mail" id="mail">
+
+            <label for="dateNaiss">Date de naissance <span class="Rouge">*</span></label>
+            <input type="date" name="dateNaiss" id="dateNaiss">
+
+            <button class="buttonCreerUser" type="submit">
+                Créer
+            </button>
+        </form>
+
+
+
+    </div>
+
+    <?php
+    include "fichierCommuns/footer.php";
+    ?>
+</div>
+</body>
+</html>
