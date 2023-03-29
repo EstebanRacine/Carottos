6f7d41f1a4021f5f09f4f6885e2de83fc0265962
Esteban Racine <esteban.racineecole@gmail.com>
2023-03-29 14:04:37 +0200

    JS pour affichage paiement

--- fichierCommuns/affichageTypePaiement.js
+++ fichierCommuns/affichageTypePaiement.js
@@ -1,29 +1,39 @@
+//
+// function showDivPaie(oEvent){
+//     var sVal = this.value,
+//         sIdCible = 'Paiement'+sVal,
+//         oForm = this.form,
+//         sClass = "affichageInfosPaiementYes",
+//         // Je recupère les select-cible qui sont dans le form
+//         // je ne parcours pas tout le DOM en faissant document....
+//         aDivs = oForm.getElementsByClassName("affichageInfosPaiement");
+//     for(var i = 0; i < aDivs.length; i++){
+//         var oEle = aDivs[i];
+//         if(oEle.id === sIdCible){
+//             oEle.classList.add(sClass)
+//         }else{
+//             oEle.classList.remove(sClass)
+//         }
+//     }
+// }
+//
+// //Quand le DOm est dispo
+// document.addEventListener('DOMContentLoaded',function(){
+//     var oForm = document.forms['formulaireValidation'];
+//     oForm['paiement'].addEventListener('change',showDivPaie);
+// });
 
-function showDivPaie(oEvent){
-    var sVal = this.value,
-        sIdCible = 'Paiement'+sVal,
-        oForm = this.form,
-        sClass = "affichageInfosPaiementYes",
-        // Je recupère les select-cible qui sont dans le form
-        // je ne parcours pas tout le DOM en faissant document....
-        aDivs = oForm.getElementsByClassName("affichageInfosPaiement");
-    for(var i = 0; i < aDivs.length; i++){
-        var oEle = aDivs[i];
-        if(oEle.id === sIdCible){
-            oEle.classList.add(sClass)
-        }else{
-            oEle.classList.remove(sClass)
-        }
-    }
+
+if (document.getElementById('paypalRadio').checked){
+    document.getElementById('Paiementpaypal').className('paypaldiv');
+}else{
+    document.getElementById('Paiementpaypal').className('affichageInfosPaiement')
 }
 
-//Quand le DOm est dispo
-document.addEventListener('DOMContentLoaded',function(){
-    var oForm = document.forms['formulaireValidation'];
-    oForm['paiement'].addEventListener('change',showDivPaie);
-});
-
-
-if ()
+if (document.getElementById('creditCardRadio').checked){
+    document.getElementById('Paiementcredit').className('creditDiv');
+}else{
+    document.getElementById('Paiementcredit').className('affichageInfosPaiement')
+}
 
 
