4e993b6f3187bf306db5a6e4c91e5e539a30166c
Esteban Racine <esteban.racineecole@gmail.com>
2023-04-04 10:40:59 +0200

    Test Push

--- fichierCommuns/affichageTypePaiement.js
+++ fichierCommuns/affichageTypePaiement.js
@@ -1,28 +1,3 @@
-//
-// function showDivPaie(oEvent){
-//     var sVal = this.value,
-//         sIdCible = 'Paiement'+sVal,
-//         oForm = this.form,
-//         sClass = "affichageInfosPaiementYes",
-//         // Je recupère les select-cible qui sont dans le form
-//         // je ne parcours pas tout le DOM en faissant document....
-//         aDivs = oForm.getElementsByClassName("affichageInfosPaiement");
-//     for(var i = 0; i < aDivs.length; i++){
-//         var oEle = aDivs[i];
-//         if(oEle.id === sIdCible){
-//             oEle.classList.add(sClass)
-//         }else{
-//             oEle.classList.remove(sClass)
-//         }
-//     }
-// }
-//
-// //Quand le DOm est dispo
-// document.addEventListener('DOMContentLoaded',function(){
-//     var oForm = document.forms['formulaireValidation'];
-//     oForm['paiement'].addEventListener('change',showDivPaie);
-// });
-
 function choixPaiement() {
     if (document.getElementById('paypalRadio').checked) {
         document.getElementById('PaiementPaypal').className='paypalDiv';
@@ -48,3 +23,4 @@ function choixPaiement() {
         document.getElementById('PaiementApplePay').className='affichageInfosPaiement'
     }
 }
+
